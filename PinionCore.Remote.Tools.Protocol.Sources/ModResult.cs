31b0239b8c0de4627fb1ede6a665bd74c7881826
jc <jiowchern@gmail.com>
2024-12-04 02:40:16 +0800

    add debug files

---
+++ PinionCore.Remote.Tools.Protocol.Sources/ModResult.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+namespace PinionCore.Remote.Tools.Protocol.Sources
+{
+    public struct ModResult
+    {
+        public System.Collections.Generic.IEnumerable<TypeSyntax> TypesOfSerialization;
+        public ClassDeclarationSyntax Type;
+        public Microsoft.CodeAnalysis.CSharp.Syntax.BlockSyntax[] UnprocessedBlocks;
+        public Dictionary<EventDeclarationSyntax, int> EventIds;
+
+        public System.Collections.Generic.IEnumerable<T> GetSyntaxs<T>() where T : Microsoft.CodeAnalysis.SyntaxNode
+        {
+            foreach (BlockSyntax block in UnprocessedBlocks)
+            {
+                Microsoft.CodeAnalysis.SyntaxNode node = block;
+
+                while (node != null && node.GetType() != typeof(T))
+                {
+                    node = node.Parent;
+                }
+                var ret = node as T;
+                if (ret != null)
+                    yield return ret;
+            }
+        }
+
+
+
+
+
+    }
+
+
+}
