8a5ffbd44821aecb3f297e227913ddd2b3c86437
jc <jiowchern@gmail.com>
2025-09-21 06:46:59 +0800

    清理測試代碼，移除不必要的日誌輸出
    本次提交主要進行了以下更改：
    - 移除了多個不必要的 `using` 陳述式。
    - 簡化了 `Test` 方法，移除了測試輸出相關的代碼。
    - 減少了對 `System.IO.File.WriteAllText` 的調用，以降低文件寫入操作。
    - 移除了 `gameAgent.ExceptionEvent` 的事件處理器，簡化異常處理邏輯。
    - 在 `gameUpdateTask` 和 `_ToStream` 方法中，移除了多餘的日誌輸出。

--- PinionCore.Remote.Gateway.Test/GatewayUserListenerTests.cs
+++ PinionCore.Remote.Gateway.Test/GatewayUserListenerTests.cs
@@ -1,10 +1,7 @@
 ﻿using System;
-using System.IO;
 using System.Linq;
 using System.Reactive.Linq;
-using NUnit;
 using NUnit.Framework;
-using NUnit.Framework.Internal.Execution;
 using PinionCore.Network;
 using PinionCore.Remote.Gateway.GatewayUserListeners;
 using PinionCore.Remote.Gateway.Protocols;
@@ -21,7 +18,6 @@ namespace PinionCore.Remote.Gateway.Tests
         [NUnit.Framework.Test,Timeout(10000)]
         public async System.Threading.Tasks.Task Test()
         {
-            NUnit.Framework.TestContext.Progress.WriteLine("GatewayUserListenerTests.Test start");
             var pool = PinionCore.Memorys.PoolProvider.Shared;
             var gameEntry = new GameEntry();
             var listener = new PinionCore.Remote.Gateway.GatewayUserListeners.GatewayUserListener();
@@ -49,28 +45,15 @@ namespace PinionCore.Remote.Gateway.Tests
                           from user_ in gpi.UserNotifier.Base.SupplyEvent()
                           where user_.Id == joinId
                           select user_;
-            var users = new System.Collections.Generic.List<IUser>();
-            var logPath = Path.Combine(AppContext.BaseDirectory, "before_user.log");
-            System.IO.File.WriteAllText(logPath, "waiting");
             var user = await userObs.FirstAsync();
-            System.IO.File.WriteAllText(Path.Combine(AppContext.BaseDirectory, "user_type.log"), user.GetType().AssemblyQualifiedName ?? "null");
 
             var gameAgent = new PinionCore.Remote.Ghost.Agent(gameProtocol, new Remote.Serializer(gameProtocol.SerializeTypes), new PinionCore.Remote.InternalSerializer(), pool) as IAgent;
             gameAgent.Enable(_ToStream(user));
-            gameAgent.HandleMessage();
-            gameAgent.HandlePackets();
-            gameAgent.ExceptionEvent += ex =>
-            {
-                System.IO.File.AppendAllText(Path.Combine(AppContext.BaseDirectory, "game_exception.log"), ex.ToString());
-            };
-           
             var gameUpdateTaskEnable = true;
             var gameUpdateTask = System.Threading.Tasks.Task.Run(() => {
                 while (gameUpdateTaskEnable)
                 {
                     System.Threading.Thread.Sleep(1);
-
-                    File.AppendAllText(Path.Combine(AppContext.BaseDirectory, "game_loop.log"), "tick\n");
                     gameAgent.HandleMessage();
                     gameAgent.HandlePackets();
                 }
@@ -99,10 +82,6 @@ namespace PinionCore.Remote.Gateway.Tests
 
         private IStreamable _ToStream(IUser user)
         {
-            var logPath = Path.Combine(AppContext.BaseDirectory, "to_stream.log");
-            var userId = user.Id.Value;
-            var hasBridge = UserStreamRegistry.TryGet(userId, out _);
-            File.AppendAllText(logPath, $"user id={userId} streamable={(user is IStreamable)} bridge={hasBridge}\n");
             if (user is IStreamable directStream)
             {
                 return directStream;
