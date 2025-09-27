db16c67b7464fc499af9132a56761eaa2a0c9a69
jc <jiowchern@gmail.com>
2025-09-28 06:13:16 +0800

    重構 ProviderRegistry 並新增單元測試
    在 `Registry.cs` 中重構 `ProviderRegistry` 類別，簡化變數命名，移除不必要的事件，並新增事件處理機制。實作 `Connect` 方法的錯誤處理，新增 `RemoveSession` 和 `_EnsureNotDisposed` 方法以管理資源釋放。新增 `ProviderSession` 類別以管理提供者的生命週期。
    
    在 `RegistrysTests.cs` 中新增 `RegistrysTests` 測試類別，實作 `ProviderRegistry_Connect_EventHandling_Test` 方法以驗證連接事件的正確性，並確保在測試結束後正確釋放資源。

---
+++ PinionCore.Remote.Gateway.Test/RegistrysTests.cs
@@ -0,0 +1,42 @@
+﻿using System.Net;
+using NUnit.Framework;
+using PinionCore.Remote.Gateway.Servers;
+using PinionCore.Remote.Soul;
+
+
+namespace PinionCore.Remote.Gateway.Tests
+{
+    public class RegistrysTests
+    {
+        [NUnit.Framework.Test, Timeout(10000)]
+        public async System.Threading.Tasks.Task ProviderRegistry_Connect_EventHandling_Test()
+        {
+
+            var hub1 = new GatewayServerServiceHub();
+            var tcpListener = new PinionCore.Remote.Server.Tcp.Listener();
+            var port1 = PinionCore.Network.Tcp.Tools.GetAvailablePort();
+            IListenable listenable = tcpListener;
+            listenable.StreamableEnterEvent += hub1.Source.Join;
+            listenable.StreamableLeaveEvent += hub1.Source.Leave;
+            tcpListener.Bind(port1);
+
+            var registry = new PinionCore.Remote.Gateway.Registrys.ProviderRegistry();
+
+            var mre1 = new System.Threading.ManualResetEvent(false);
+            registry.ProviderAddedEvent += (g,s) => {
+                mre1.Set();
+            };
+            registry.Connect(1 ,  IPEndPoint.Parse($"127.0.0.1:{port1}"));
+
+            mre1.WaitOne(2000);
+
+            // release...
+            listenable.StreamableEnterEvent -= hub1.Source.Join;
+            listenable.StreamableLeaveEvent -= hub1.Source.Leave;
+            tcpListener.Close();
+
+            
+        }
+    }
+}
+
